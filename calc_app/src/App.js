7bdbace96f56464326fb4d910efed3ec6caee71c
Eben <kwabenat679@gmail.com>
2025-02-03 00:12:49 +0000

    added css styling to calc app, in output, and operands

--- calc_app/src/App.js
+++ calc_app/src/App.js
@@ -4,12 +4,11 @@ function App () {
   return (
     <div className="calculator-grid">
       <div className="output">
-      <div className="previous-operand"></div>
-      <div className="current-operand"></div>
+      <div className="previous-operand">123</div>
+      <div className="current-operand">123</div>
     </div>
     <button className="span-two">AC</button>
     <button>DEL</button>
-    <button>=</button>
     <button>÷</button>
     <button>1</button>
     <button>2</button>
