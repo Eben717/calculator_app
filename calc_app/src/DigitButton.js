4ad9b809754a05a9621110ae87229c5ea2a8581b
Eben <kwabenat679@gmail.com>
2025-02-04 23:51:30 +0000

    added a digitbutton component

---
+++ calc_app/src/DigitButton.js
@@ -0,0 +1,6 @@
+import { ACTIONS } from "./App"
+
+export default function DigitButton({ dispatch, digit }) {
+    <button 
+    onClick={() => dispatch ({ type: ACTIONS.ADD_DIGIT, payload})}>{digit}</button>
+}
