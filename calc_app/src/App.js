79aec16c09769242b471c7a6372eb8db480088e1
Eben <kwabenat679@gmail.com>
2025-02-08 12:37:53 +0000

    added function for evaluate previous and current operand with operation signs

--- calc_app/src/App.js
+++ calc_app/src/App.js
@@ -40,10 +40,41 @@ function reducer(state, { type, payload }) {
           }
         };
 
+        return {
+          ...state,
+          previousOperand: evaluate(state),
+          operation: payload.operation,
+          currentOperand: null
+        }
+        
+
       case ACTIONS.CLEAR:
       return {};
     default:
       return state;
+  }
+  
+
+  function evaluate ({currentOperand, previousOperand, operation}) {
+    const prev = parseFloat(previousOperand)
+    const current = parseFloat(currentOperand)
+    if (isNaN(prev || isNaN(current))) return ""
+    let computation = ""
+    switch (operation) {
+      case "+":
+        computation = prev + current
+        break
+      case "-":
+        computation = prev - current
+        break
+      case "*":
+        computation = prev * current
+        break
+      case "÷":
+        computation = prev / current
+        break
+    }
+    return computation.toString()
   }
   
 }
